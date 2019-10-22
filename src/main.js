3dd983e660873349b24954cab3546b278e022241
Valentin Nicolae <nicolae9931@yahoo.com>
2019-10-22 19:23:13 +0100

    First problem: Nan

---
+++ src/main.js
@@ -0,0 +1,19 @@
+function calc() {
+    var vf = parseInt(document.querySelector("#value1").value);
+    var vs = parseInt(document.querySelector("#value2").value);
+    var op = document.querySelector("#operator").value;
+    var calculate;
+
+    if (op == 'add') {
+        calculate = vf + vs;
+    } else if (op == 'min') {
+        calculate = vf - vs;
+    } else if (op == 'div' ) {
+        calculate = vf / vs;
+    } else if (op == 'mul') {
+        calculate = vf * vs;
+    }
+     console.log(vs);
+     console.log(vf);
+   // document.querySelector("#result").innerHTML = calculate;
+}
